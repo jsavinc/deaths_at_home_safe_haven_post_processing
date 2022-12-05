495d206ea0aa03aa428dd9d14b3faf234412c9e7
Jan Savinc <j.savinc@napier.ac.uk>
2022-12-05 17:04:31 +0000

    added to helper functions

--- FUNCTIONS.R
+++ FUNCTIONS.R
@@ -1,7 +1,13 @@
 ## FUNCTIONS.R
 ## This includes various functions for use in the post-processing of the deaths at home data, after they've been exported from the Safe Haven
 
-require(magrittr)
+require(tidyverse)
+
+
+# Constants ---------------------------------------------------------------
+
+## For ordering factors etc.
+order_place_of_death <- c("Hospital", "Home & non-institution", "Care home & other")
 
 # Parse N (%) notation ----------------------------------------------------
 
@@ -11,7 +17,6 @@ require(magrittr)
 ## the N and proportion from a single column, removing the column in the process
 
 parse_n_prop <- function(data_tbl, col_n_prop, remove = TRUE) {
-  require(tidyverse)
   quo_col_n_prop <- enquo(col_n_prop)
   data_tbl %>%
     tidyr::extract(
@@ -23,3 +28,38 @@ parse_n_prop <- function(data_tbl, col_n_prop, remove = TRUE) {
     dplyr::mutate(n = parse_number(n), prop = parse_number(prop) / 100)
 }
 
+
+# Convert annual columns to long format -----------------------------------
+
+pivot_annual_to_long <- function(data_tbl) {
+  data_tbl %>%
+    tidyr::pivot_longer(cols = matches("\\d{4}\\-\\d{2}"), names_to = "cohort_year", values_to = "n_prop")
+}
+
+
+# Calculate N (%) from n and prop -----------------------------------------
+
+calculate_n_prop <- function(n, prop, accuracy = 0.1) {
+  if_else(
+    ## replace with NA where there are 0s
+    condition = is.na(n)|is.na(prop),
+    true = NA_character_,
+    false = 
+      paste0(scales::comma(x = n),  # thousands separator
+             " (",
+             scales::percent(x = prop, accuracy = accuracy),  # pretty percent
+             ")"
+    )
+  )
+}
+
+
+
+# Replace percentage with 100% --------------------------------------------
+
+## This is a daft helper that finds a percentage figure and replaces with 100%
+replace_percent_with_100 <- function(some_string) {
+  str_replace(string = some_string,
+              pattern = "\\d+\\.{0,1}\\d*\\%",
+              replacement = "100%")
+}
