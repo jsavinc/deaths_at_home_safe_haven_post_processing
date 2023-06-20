b152d9adaf7d2424699d58a6ca2ce81f3a6bbf3c
Jan Savinc <j.savinc@napier.ac.uk>
2023-06-20 12:24:43 +0100

    finished importing data from exports, now need to format a table!

--- descriptive_paper_1_scripts.r
+++ descriptive_paper_1_scripts.r
@@ -37,25 +37,71 @@ sex_by_pod <-
   pivot_annual_to_long() %>%
   parse_n_prop(col_n_prop = n_prop)
 
-marstat_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/marstat_by_pod.csv")
+marstat_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/marstat_by_pod.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop)
 
-ethnicity_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/ethnicity_by_pod.csv")
+ethnicity_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/ethnicity_by_pod.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop)
 
 ## rounded to nearest 10
-simd_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/imgs2022_preliminary_results/place_of_death_by_simd_and_cohort.csv")
+simd_by_pod <-
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/imgs2022_preliminary_results/place_of_death_by_simd_and_cohort.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop)
 
-ur_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/imgs2022_preliminary_results/place_of_death_by_ur_and_cohort.csv")
+ur_by_pod <-
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/imgs2022_preliminary_results/place_of_death_by_ur_and_cohort.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop)
 
-num_cod_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/service_usage_by_cause_of_death/deaths_pod_cod_count.csv")
-num_cod_categorised_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/cod_count_categorised_by_pod.csv")
+num_cod_by_pod <-
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/service_usage_by_cause_of_death/deaths_pod_cod_count.csv") %>%
+  pivot_annual_to_long(new_col = "mean_ci") %>%
+  parse_m_ci(col_m_ci = mean_ci)
+
+num_cod_categorised_by_pod <- 
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/cod_count_categorised_by_pod.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop)
 
 ## this includes both number of comorbidities & elixhauser index
-comorb_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/service_usage_by_cause_of_death/deaths_pod_comorb_index.csv")
-num_comorb_categorised_by_pod <- read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/comorb_count_categorised_by_pod.csv")
+comorb_by_pod <- 
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/service_usage_by_cause_of_death/deaths_pod_comorb_index.csv") %>%
+  pivot_annual_to_long(new_col = "mean_ci") %>%
+  parse_m_ci(col_m_ci = mean_ci)
+## number of comorbidities, categorised
+num_comorb_categorised_by_pod <- 
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/exploratory_descriptives/comorb_count_categorised_by_pod.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop)
 
 ## recalculated from the breakdown by sex
 pall_care_needs_by_pod <- 
-  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/palliative_care_needs_exploration/palliative_estimates_murtaugh_upper_mid_by_cohort_pod_sex.csv")
-  # TODO: aggregate over sex!
+  read_csv(file = "X:/R2090/2021-0312 Deaths at home/safe_haven_exports/palliative_care_needs_exploration/palliative_estimates_murtaugh_upper_mid_by_cohort_pod_sex.csv") %>%
+  pivot_annual_to_long() %>%
+  parse_n_prop(col_n_prop = n_prop) %>%
+  mutate(denominator = round(n / prop)) %>%  # infer denominator from proportions
+  group_by(cat_place_of_death, val_cohort_year) %>%
+  summarise(
+    n = sum(n), 
+    denominator = sum(denominator), 
+    .groups="drop"
+    ) %>%
+  mutate(prop = n / denominator)
 
 # TODO: import the breakdowns of pall care needs by other variables if needed
+
+pal_care_needs_by_pod_simd <- NULL
+
+table1 <-
+  bind_rows(
+    age_by_pod %>% 
+      mutate(measure = "Age, M (SD)", value = glue::glue("{round(mean,2)} ({round(sd,2)})")) %>%
+      select(measure, val_cohort_year, cat_place_of_death, value),
+    sex_by_pod %>%
+      mutate(measure = "Female decedents, N (%)", value = glue::glue("{scales::comma(n)} ({scales::label_percent(accuracy = 0.1)(prop)})")) %>%
+      select(measure, val_cohort_year, cat_place_of_death, value),
+    
+  )
